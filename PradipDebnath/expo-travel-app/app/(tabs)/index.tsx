4c78ddfd5fc1131e4b07005c98656b52f34211b1
Yooodh <eogus2604@hanmail.net>
2024-08-23 00:08:23 +0900

    v0.2

--- PradipDebnath/expo-travel-app/app/(tabs)/index.tsx
+++ PradipDebnath/expo-travel-app/app/(tabs)/index.tsx
@@ -1,16 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
+import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
+import { useHeaderHeight } from '@react-navigation/elements';
 
 const Page = () => {
+  const headerHeight = useHeaderHeight();
   return (
-    <View style={styles.container}>
-      <Text>Page</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: '',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {}} style={{ marginLeft: 20 }}>
+              <Image
+                source={{
+                  uri: 'https://xsgames.co/randomusers/avatar.php?g=female',
+                }}
+                style={{ width: 40, height: 40, borderRadius: 10 }}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                marginRight: 20,
+                backgroundColor: Colors.white,
+                padding: 10,
+                borderRadius: 10,
+                shadowColor: '#171717',
+                shadowOffset: { width: 2, height: 4 },
+                shadowOpacity: 0.2,
+                shadowRadius: 3,
+              }}
+            >
+              <Ionicons name='notifications' size={20} color={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View style={[styles.container, { paddingTop: headerHeight }]}>
+        <Text>Explore The Beautiful World!</Text>
+      </View>
+    </>
   );
 };
 
 export default Page;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: Colors.bgColor,
+  },
+  headingTxt: {
+    fontSize: 28,
+    fontWeight: '800',
+    color: Colors.black,
+    marginTop: 10,
+  },
 });
