a56ed4be499b4ce3214aa176149532cd844fda0c
Yooodh <eogus2604@hanmail.net>
2024-11-04 01:01:49 +0900

    v0.7

--- PradipDebnath/expo-ecoomerce-app/components/TabBarButton.tsx
+++ PradipDebnath/expo-ecoomerce-app/components/TabBarButton.tsx
@@ -19,6 +19,13 @@ const TabBarButton = (props: Props) => {
       onLongPress={onLongPress}
       style={styles.tabbarBtn}
     >
+      {routeName == 'cart' && (
+        //  Cart Badge is Here
+        <View style={styles.badgeWrapper}>
+          <Text style={styles.badgeText}>3</Text>
+        </View>
+      )}
+
       {icon[routeName]({
         color: isFocused ? Colors.primary : Colors.black,
       })}
@@ -35,5 +42,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     gap: 5,
+  },
+  badgeWrapper: {
+    position: 'absolute',
+    backgroundColor: Colors.highlight,
+    top: -5,
+    right: 20,
+    paddingVertical: 2,
+    paddingHorizontal: 6,
+    borderRadius: 10,
+    zIndex: 10,
+  },
+  badgeText: {
+    color: Colors.black,
+    fontSize: 12,
   },
 });
