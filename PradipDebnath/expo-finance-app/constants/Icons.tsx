20c02fbe753dce348634aa95c81f4b7f75519c42
Yooodh <eogus2604@hanmail.net>
2024-11-15 16:36:46 +0900

    v0.2

---
+++ PradipDebnath/expo-finance-app/constants/Icons.tsx
@@ -0,0 +1,25 @@
+import DollarIcon from '@/assets/icons/usd-circle.svg';
+import WalletAddMoneyIcon from '@/assets/icons/wallet-income.svg';
+import WalletCardIcon from '@/assets/icons/wallet.svg';
+import AmazonIcon from '@/assets/icons/cc-amazon-pay.svg';
+import UberIcon from '@/assets/icons/usd-circle.svg';
+import AirbnbIcon from '@/assets/icons/airbnb.svg';
+import DollarSignIcon from '@/assets/icons/money-transfer-coin-arrow.svg';
+import FigmaIcon from '@/assets/icons/figma.svg';
+import ShoppingCartIcon from '@/assets/icons/shopping-cart.svg';
+import SpotifyIcon from '@/assets/icons/spotify.svg';
+import NetflixIcon from '@/assets/icons/netflix.svg';
+
+export {
+  DollarIcon,
+  WalletAddMoneyIcon,
+  WalletCardIcon,
+  AmazonIcon,
+  UberIcon,
+  AirbnbIcon,
+  DollarSignIcon,
+  FigmaIcon,
+  ShoppingCartIcon,
+  SpotifyIcon,
+  NetflixIcon,
+};
