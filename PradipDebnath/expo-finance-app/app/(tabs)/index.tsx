108749a8f106ea2cf51ef8075bb2804571ece60b
Yooodh <eogus2604@hanmail.net>
2024-11-17 01:28:36 +0900

    v0.3

---
+++ PradipDebnath/expo-finance-app/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+const Page = () => {
+  return (
+    <View>
+      <Text>Page</Text>
+    </View>
+  );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({});
---
+++ PradipDebnath/expo-finance-app/app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  );
+};
+
+export default profile;
+
+const styles = StyleSheet.create({});
