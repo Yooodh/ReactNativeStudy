eb472ee95ab403aa74797df72b539903227fb174
Yooodh <eogus2604@hanmail.net>
2024-11-18 00:54:39 +0900

    v0.4

--- PradipDebnath/expo-finance-app/app/(tabs)/_layout.tsx
+++ PradipDebnath/expo-finance-app/app/(tabs)/_layout.tsx
@@ -1,9 +1,82 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { Tabs } from 'expo-router';
+import { AntDesign, FontAwesome, SimpleLineIcons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
 
 const _layout = () => {
-  return <Tabs />;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: Colors.grey,
+          position: 'absolute',
+          bottom: 40,
+          justifyContent: 'center',
+          alignSelf: 'center',
+          height: 63,
+          marginHorizontal: 120,
+          paddingHorizontal: 10,
+          paddingVertical: 8,
+
+          paddingBottom: 8,
+          borderRadius: 40,
+        },
+        tabBarShowLabel: false,
+        tabBarInactiveTintColor: '#999',
+        tabBarActiveTintColor: Colors.white,
+      }}
+    >
+      <Tabs.Screen
+        name='index'
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <View
+              style={{
+                padding: 12,
+                borderRadius: 30,
+                backgroundColor: focused ? Colors.tintColor : Colors.grey,
+              }}
+            >
+              <SimpleLineIcons name='pie-chart' size={18} color={color} />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='transactions'
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <View
+              style={{
+                padding: 12,
+                borderRadius: 30,
+                backgroundColor: focused ? Colors.tintColor : Colors.grey,
+              }}
+            >
+              <AntDesign name='swap' size={18} color={color} />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <View
+              style={{
+                padding: 12,
+                borderRadius: 30,
+                backgroundColor: focused ? Colors.tintColor : Colors.grey,
+              }}
+            >
+              <FontAwesome name='user-o' size={18} color={color} />
+            </View>
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default _layout;
