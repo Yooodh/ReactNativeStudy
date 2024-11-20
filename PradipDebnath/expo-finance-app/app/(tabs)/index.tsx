73450f3ab22586592d8bfd65c1586bb6a7f98353
Yooodh <eogus2604@hanmail.net>
2024-11-21 00:49:58 +0900

    v0.7

--- PradipDebnath/expo-finance-app/app/(tabs)/index.tsx
+++ PradipDebnath/expo-finance-app/app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import React from 'react';
 import { NetflixIcon } from '@/constants/Icons';
 import Colors from '@/constants/Colors';
 import { Stack } from 'expo-router';
 import Header from '@/components/Header';
+import { PieChart } from 'react-native-gifted-charts';
 
 const Page = () => {
+  const pieData = [
+    {
+      value: 47,
+      color: Colors.tintColor,
+      focused: true,
+      text: '47%',
+    },
+    { value: 40, color: Colors.blue, text: '40%' },
+    { value: 16, color: Colors.white, text: '16%' },
+    { value: 3, color: '#FFA5BA', gradientCenterColor: '#FF7F97' },
+  ];
   return (
     <>
       <Stack.Screen
@@ -13,8 +25,58 @@ const Page = () => {
           header: () => <Header />,
         }}
       />
-      <View style={styles.container}>
-        <Text>Page</Text>
+      {/* <View style={[styles.container, { paddingTop: 40 }]}> */}
+      <View style={[styles.container, { paddingTop: 80 }]}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ gap: 10 }}>
+              <Text style={{ color: Colors.white, fontSize: 16 }}>
+                My <Text style={{ fontWeight: 700 }}>Expenses</Text>
+              </Text>
+              <Text
+                style={{ color: Colors.white, fontSize: 36, fontWeight: 700 }}
+              >
+                $1475.<Text style={{ fontSize: 22, fontWeight: 400 }}>00</Text>
+              </Text>
+            </View>
+            <View style={{ paddingVertical: 20, alignItems: 'center' }}>
+              <PieChart
+                data={pieData}
+                donut
+                showGradient
+                sectionAutoFocus
+                // focusOnPress
+                semiCircle
+                radius={70}
+                innerRadius={55}
+                innerCircleColor={Colors.black}
+                centerLabelComponent={() => {
+                  return (
+                    <View
+                      style={{ justifyContent: 'center', alignItems: 'center' }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 22,
+                          color: 'white',
+                          fontWeight: 'bold',
+                        }}
+                      >
+                        47%
+                      </Text>
+                    </View>
+                  );
+                }}
+              />
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </>
   );
