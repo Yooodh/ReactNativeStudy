49d59eaf2ae9fb2d6a3e2d8e72b43f4b09f34872
Yooodh <eogus2604@hanmail.net>
2024-11-28 22:42:54 +0900

    v1.2

--- PradipDebnath/expo-finance-app/components/SpendingBlock.tsx
+++ PradipDebnath/expo-finance-app/components/SpendingBlock.tsx
@@ -2,47 +2,50 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { SpendingType } from '@/types';
 import Colors from '@/constants/Colors';
-import { DollarIcon } from '@/constants/Icons';
+import {
+  AirbnbIcon,
+  AmazonIcon,
+  DollarIcon,
+  FigmaIcon,
+  NetflixIcon,
+  ShoppingCartIcon,
+  SpotifyIcon,
+} from '@/constants/Icons';
 
 const SpendingBlock = ({ spendingList }: { spendingList: SpendingType[] }) => {
+  let icon = <DollarIcon width={22} height={22} color={Colors.white} />;
+
   return (
-    <View style={{ marginVertical: 20, alignItems: 'flex-start' }}>
-      <Text style={{ color: Colors.white, fontSize: 16 }}>
+    <View style={styles.spendingSectionWrapper}>
+      <Text style={styles.sectionTitle}>
         July <Text style={{ fontWeight: '700' }}>Spending</Text>
       </Text>
 
       {spendingList.map((item) => {
+        if (item.name == 'AirBnB Rent') {
+          icon = <AirbnbIcon width={22} height={22} color={Colors.white} />;
+        } else if (item.name == 'Netflix') {
+          icon = <NetflixIcon width={22} height={22} color={Colors.white} />;
+        } else if (item.name == 'Spotify') {
+          icon = <SpotifyIcon width={22} height={22} color={Colors.white} />;
+        } else if (item.name == 'Amazon') {
+          icon = <AmazonIcon width={22} height={22} color={Colors.white} />;
+        } else if (item.name == 'Figma') {
+          icon = <FigmaIcon width={22} height={22} color={Colors.white} />;
+        } else if (item.name == 'Online Shopping') {
+          icon = (
+            <ShoppingCartIcon width={22} height={22} color={Colors.white} />
+          );
+        }
         return (
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginVertical: 10,
-            }}
-            key={item.id}
-          >
-            <View
-              style={{
-                backgroundColor: Colors.grey,
-                padding: 15,
-                borderRadius: 50,
-              }}
-            >
-              <DollarIcon width={22} height={22} color={Colors.white} />
-            </View>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}
-            >
+          <View style={styles.spendingWrapper} key={item.id}>
+            <View style={styles.iconWrapper}>{icon}</View>
+            <View style={styles.textWrapper}>
               <View style={{ gap: 5 }}>
-                <Text style={{ color: Colors.white }}>{item.name}</Text>
+                <Text style={styles.itemName}>{item.name}</Text>
                 <Text style={{ color: Colors.white }}>{item.date}</Text>
               </View>
-              <Text style={{ color: Colors.white }}>${item.amount}</Text>
+              <Text style={styles.itemName}>${item.amount}</Text>
             </View>
           </View>
         );
@@ -53,4 +56,36 @@ const SpendingBlock = ({ spendingList }: { spendingList: SpendingType[] }) => {
 
 export default SpendingBlock;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  spendingSectionWrapper: {
+    marginVertical: 20,
+    alignItems: 'flex-start',
+  },
+  sectionTitle: {
+    color: Colors.white,
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  spendingWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  iconWrapper: {
+    backgroundColor: Colors.grey,
+    padding: 15,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  textWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  itemName: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
